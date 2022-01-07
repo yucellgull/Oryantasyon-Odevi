0540250b70b8c368752d9aefc01e5d8a314a1cfd
Yücel <yucellgull@hotmail.com>
2022-01-07 10:38:09 +0300

    Add files via upload

---
+++ soru1.java
@@ -0,0 +1,12 @@
+package Java.soru1;
+import java.util.Scanner;
+public class soru1{
+    public static void main(String[] args) {
+        String isim;
+        Scanner klavye = new Scanner(System.in);
+        System.out.print("Lütfen İsminizi Giriniz:");
+        isim = klavye.nextLine();
+           
+        System.out.println("Merhaba " + isim);
+    }
+  }
