3818d4e4ca7680ab106dd0b1e67e024142df5f66
Yücel <yucellgull@hotmail.com>
2022-01-09 06:29:29 +0300

    Add files via upload

---
+++ Soru1.java
@@ -0,0 +1,14 @@
+//kullanıcıdan ismini alarak "Merhaba isim" yazan program
+
+package java.soru1;
+import java.util.Scanner;
+public class soru1{
+    public static void main(String[] args) {
+        String isim;
+        Scanner klavye = new Scanner(System.in);
+        System.out.print("Lütfen İsminizi Giriniz:");
+        isim = klavye.nextLine();
+           
+        System.out.println("Merhaba " + isim);
+    }
+  }
